6b7887dea7bce77edfc866ca25a98fe462db14ed
fiantyogalihp <fiantiogalih@gmail.com>
2022-12-04 03:33:18 +0700

    dependency major upgrade to v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/fiantyogalihp/app-say-hello
 
 go 1.18
 
-require github.com/fiantyogalihp/go-say-hello v1.0.1
+require github.com/fiantyogalihp/go-say-hello/v2 v2.0.0
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/fiantyogalihp/go-say-hello"
+	go_say_hello "github.com/fiantyogalihp/go-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello("Slamet"))
+	fmt.Println(go_say_hello.SayHello("fiantyo", "galih"))
 }
